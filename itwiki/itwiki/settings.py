aea189392a79ca414ffa9141fe809aab1d7dc523
danhjun <danieljun3@gmail.com>
2024-04-16 16:01:06 -0400

    4:01 PM

--- itwiki/itwiki/settings.py
+++ itwiki/itwiki/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+%_4rihemmk=qz39lnac0!b4-pp@#=a6pjh9t221ecu)xvca)y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
