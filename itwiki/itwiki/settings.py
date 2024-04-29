c1e0ae8f0cbd3090e35cd931450735148be13af9
danhjun <danieljun3@gmail.com>
2024-04-29 17:10:51 -0400

    5:10 PM

--- itwiki/itwiki/settings.py
+++ itwiki/itwiki/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '3.134.109.119']
+ALLOWED_HOSTS = ['127.0.0.1', '3.14.145.147', 'itwiki.us', 'www.itwiki.us']
 
 # Application definition
 
